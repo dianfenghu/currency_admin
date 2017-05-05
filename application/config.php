483137ef523d8a219270d6f841210c5d51353bbb
wuwu <15093565100@163.com>
2017-05-05 20:55:09 +0800

    修改

--- application/config.php
+++ application/config.php
@@ -140,7 +140,7 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__PUBLIC__' => '/web1/public'
+        '__PUBLIC__' => ''
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
