f65ba38da903205273cbae14ffc53713467f7bd0
chenteng <71125@163.com>
2017-05-10 21:43:48 +0800

    文章管理

---
+++ application/common/validate/Content.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\common\validate;
+use think\Validate;
+
+class Content extends Validate
+{
+	   protected $rule = [
+        'title'  =>  'require|max:25',
+        'type' =>  'require',
+        'content' =>  'require',
+       // 'photo' =>  'require',
+    ];
+
+	   
+    protected $message = [
+        'title.require' => '文章标题不能为空',
+        'type' => '请选择文章分类',
+		'content' => '文章正文不能为空',
+	//	'photo' => '请先上传文章缩略图'
+    ];
+
+
+}
